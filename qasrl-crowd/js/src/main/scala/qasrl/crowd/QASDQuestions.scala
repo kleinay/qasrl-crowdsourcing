a52d343826c6cfbf78ec341b4f12c2ec51edc986
kleinay <ayal.s.klein@gmail.com>
2018-04-09 17:06:17 +0300

    added QASDQuestion template logic for PREP and parantheses expansion

--- qasrl-crowd/js/src/main/scala/qasrl/crowd/QASDQuestions.scala
+++ qasrl-crowd/js/src/main/scala/qasrl/crowd/QASDQuestions.scala
@@ -1,15 +1,53 @@
 package qasrl.crowd
 
+/*
+ Template syntax:
+
+*   [W]     is replaced with the target word
+*   <PREP>  is replaced with each of the prepositions
+*   (word) is generating two templates, with and without the word in parantheses (only one allowed in single template)
+
+ */
+
 case class QASDQuestions(targetWord : String, templateList : List[String]) {
   // load templates from file, instantiate them with target word
   lazy val qlist : List[String] = {
-    templateList map (_.replace(QASDQuestions.TRGT_SYMBOL, targetWord))
-    // TODO expand PREP_SYMBOL with all prepositions
+    templateList
+        .map(_.replace(QASDQuestions.TRGT_SYMBOL, targetWord))  // replace TRGT_SYMBOL
+        .map(replacePrep).flatten // expand PREP_SYMBOL with all prepositions
+        .map(handleParatheses).flatten // replace paranthese with two templates (with\out content)
+        .toSet.toList
+
   }
-  
+
+  def replacePrep(sentence : String) : List[String] = {
+    for (prep <- QASDQuestions.Prepositions)
+      yield sentence.replace(QASDQuestions.PREP_SYMBOL, prep)
+  }
+
+  def handleParatheses(sentence : String) : List[String] = {
+    val sentWithoutPrn = sentence.replaceAll("\\(.*?\\)", "");
+    val sentWithPrn = sentence.replace("(", "").replace(")", "")
+    Set(sentWithPrn, sentWithoutPrn).toList
+  }
+
+
 }
 
 object QASDQuestions {
   val TRGT_SYMBOL = "[W]"
   val PREP_SYMBOL = "<PREP>"
+
+  val Prepositions =
+    """on
+      |under
+      |with
+      |above
+      |in
+      |over
+      |for"""
+      .stripMargin
+      .split("\n")
+      .toList
+
 }
