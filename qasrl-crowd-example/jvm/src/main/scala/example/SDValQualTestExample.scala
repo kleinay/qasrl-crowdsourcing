408192eba77174b726772d108c9bd614e2332934
kleinay <ayal.s.klein@gmail.com>
2018-06-28 12:42:02 +0300

    ValTest answers- compute number of questions&answers automatically

--- qasrl-crowd-example/jvm/src/main/scala/example/SDValQualTestExample.scala
+++ qasrl-crowd-example/jvm/src/main/scala/example/SDValQualTestExample.scala
@@ -5,6 +5,10 @@ import qasrl.crowd.QualTest
 object SDValQualTestExample extends QualTest {
   override val testString = QualTest.getTestFromResource("sdvalSpanQualTest.xml")
 
+  private[this] def answerXMLs(answerKey : Map[String, String]) : String = (
+    for ((q,a) <- answerKey) yield s"${answerXML(q, a)}"
+  ).mkString("\n")
+
   private[this] def answerXML(qid: String, aid: String): String = answerXML(qid, List(aid))
   private[this] def answerXML(qid: String, aids: List[String]): String = {
     val opts = aids.map(aid => s"""
@@ -21,14 +25,20 @@ $opts
 """.trim
 
   }
+
+  // todo - move answerKey to a file (json object?) like the testXml
+  val answerKey : Map[String, String] = Map(
+    "q1" -> "q1-a2",
+    "q2" -> "q2-invalid",
+    "q3" -> "q3-a3"
+  )
+
   override val answerKeyString = s"""<?xml version="1.0" encoding="UTF-8"?>
 <AnswerKey xmlns="http://mechanicalturk.amazonaws.com/AWSMechanicalTurkDataSchemas/2005-10-01/AnswerKey.xsd">
-${answerXML("q1", "q1-a2")}
-${answerXML("q2", "q2-invalid")}
-${answerXML("q3", "q3-a3")}
+${answerXMLs(answerKey)}
 <QualificationValueMapping>
   <PercentageMapping>
-    <MaximumSummedScore>14</MaximumSummedScore>
+    <MaximumSummedScore>${answerKey.size}</MaximumSummedScore>
   </PercentageMapping>
 </QualificationValueMapping>
 </AnswerKey>
