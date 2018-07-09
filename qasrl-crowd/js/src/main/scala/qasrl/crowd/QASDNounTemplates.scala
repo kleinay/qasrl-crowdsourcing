a2ddec4080503b24e21bc4e4ec439f2273abcb68
kleinay <ayal.s.klein@gmail.com>
2018-07-09 20:53:52 +0300

    Update QASD Noun Question Templates

--- qasrl-crowd/js/src/main/scala/qasrl/crowd/QASDNounTemplates.scala
+++ qasrl-crowd/js/src/main/scala/qasrl/crowd/QASDNounTemplates.scala
@@ -8,17 +8,15 @@ object QASDNounTemplates {
       |How much [W]?
       |How many [W]?
       |[W] for how long?
-      |Who/What is/was a/an [W]?
-      |Who/What is/was [W]?
-      |Who/What is an example of [W]?
-      |Where is/was (the )[W]?
       |When is/was (the )[W]?
-      |What is/was the time of (the )[W]?
-      |What is/was (the )[W] of?
-      |What is/was (the )[W]?
-      |What is/was (the )[W] <PREP>?
+      |What is/was the time of (the) [W]?
+      |Who/What is an example of [W]?
+      |Who/What is/was (a/an/the )[W]?
+      |Where is/was (the )[W]?
+      |Who/What is/was (the )[W] <PREP>?
       |What is/was (the )[W] to do?
-      |Where is/was (the )[W] <PREP>?"""
+      |Where is/was (the )[W] <PREP>?
+      |Who/What is/was <PREP> (a/an/the ) [W]?"""
       .stripMargin
       .split("\n")
       .toList
